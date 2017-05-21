e54254d708b1082fe2f3d4a75d550b6feb76f86c
iamshiao <iamshiao@gmail.com>
2017-05-21 18:54:18 +0800

    Correct link relation setting of ClubMember

--- Toastmaster.Web.Console/Models/ClubMember.cs
+++ Toastmaster.Web.Console/Models/ClubMember.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Toastmaster.Web.Console.Models
 {
     public class ClubMember
     {
+        [Key]
+        [Column(Order = 1)]
         public int ClubId { get; set; }
+        [Key]
+        [Column(Order = 2)]
         public int MemberId { get; set; }
         public Club Club { get; set; }
         public Member Member { get; set; }
