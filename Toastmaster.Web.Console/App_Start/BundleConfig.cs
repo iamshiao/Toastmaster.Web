183159e09d5d6175c14eb2befe81a6c7bb5aa82a
Circle <iamshiao@gmail.com>
2017-05-30 14:31:52 +0800

    Install JQuery UI
    Install JQuery UI
    Set up bundle & _Layout
    Pilot datepicker & autocomplete combobox

--- Toastmaster.Web.Console/App_Start/BundleConfig.cs
+++ Toastmaster.Web.Console/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace Toastmaster.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-1.12.1.min.js",
+                        "~/Scripts/jquery-ui-combobox.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +30,10 @@ namespace Toastmaster.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jquery-ui-css").Include(
+                      "~/Content/themes/base/jquery-ui.min.css",
+                      "~/Content/jquery-ui-combobox.css"));
         }
     }
 }
